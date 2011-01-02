191478164306b8f4f977db24b48275ffe91e5edb
e.e d3si9n <eed3si9n@gmail.com>
2011-01-02 03:11:28 -0500

    search.

--- src/main/scala/twt/TwtProcessor.scala
+++ src/main/scala/twt/TwtProcessor.scala
@@ -32,11 +32,11 @@ class TwitterProcessor extends Processor {
         C.configure(conf.getPath)
 
         val token = Token(C.config.configMap("access").asMap)
-
+        project.log.debug(symbols.toString)
         symbols match {
           case Nil => usage
 
-          case Unquoted("clearautho") :: Nil =>
+          case Unquoted("clearauth") :: Nil =>
             conf.delete()
             println("OAuth credentials deleted.")
 
@@ -47,6 +47,16 @@ class TwitterProcessor extends Processor {
           case Unquoted("commit") :: Quoted(tweet) :: Nil => token map { tok =>
               commit(tweet, tok)
             } getOrElse { get_authorization(symbols) }
+          case Unquoted("ci") :: Quoted(tweet) :: Nil => token map { tok =>
+              commit(tweet, tok)
+            } getOrElse { get_authorization(symbols) }
+
+          case Unquoted("grep") :: Unquoted(q) :: Nil => grep(q)
+          case Unquoted("grep") :: Quoted(q) :: Nil => grep(q)
+          case Unquoted("search") :: Unquoted(q) :: Nil => grep(q)
+          case Unquoted("search") :: Quoted(q) :: Nil => grep(q)
+          case Unquoted("?") :: Unquoted(q)  :: Nil => grep(q)
+          case Unquoted("?") :: Quoted(q) :: Nil => grep(q)
 
           case Unquoted("pin") :: Unquoted(pin) :: Nil => get_authorization(symbols)
           case Unquoted("pin") :: Quoted(pin) :: Nil => get_authorization(symbols)
@@ -60,10 +70,11 @@ class TwitterProcessor extends Processor {
 
   def usage {
     println("usage: twt <command>")
-    println("  twt log              : prints your timeline except for RTs.")
-    println("  twt commit \"tweet!\": tweets quoted string.")
-    println("  twt pin 123456       : authorizes twt to access twitter.")
-    println("  twt clearauth        : clears the authorization.")
+    println("  twt log             : prints your timeline except for RTs.")
+    println("  twt grep #scala     : searches for #scala. also as twt ?")
+    println("  twt commit \"tweet!\" : tweets quoted string. also as twt ci.")
+    println("  twt pin 1234567     : authorizes twt to access twitter.")
+    println("  twt clearauth       : clears the authorization.")
   }
 
   def friendsTimeline(token: Token) {
@@ -73,6 +84,14 @@ class TwitterProcessor extends Processor {
       msg = Status.text(item)
       screen_name = Status.user.screen_name(item)
     } yield println("%-15s%s" format (screen_name.toString, msg) )
+  }
+
+  def grep(q: String) {
+    for {
+      item <- http(Search(q, ("show_user", "true")))
+      msg = Search.text(item)
+      from_user = Search.from_user(item)
+    } yield println("%-15s%s" format (from_user.toString, msg) )
   }
 
   def commit(tweet: String, token: Token): String =
@@ -94,7 +113,7 @@ class TwitterProcessor extends Processor {
 
       // the friends message should be the first one to come in
       for (JArray(friends) <- message \ "friends") yield
-        println("Streaming tweets as they arrive...")
+        println("streaming tweets as they arrive...")
 
       // print apparent tweet if it has text and a screen_name
       for {
@@ -113,10 +132,10 @@ class TwitterProcessor extends Processor {
       http(Auth.access_token(consumer, token, pin)) match {
         case (access_tok, _, screen_name) =>
           // nb: we're producing a message, a token type name, and the token itself
-          ("Approved! It's tweetin' time, %s." format screen_name, Some(("access", access_tok)))
+          ("approved! it's tweetin' time, %s." format screen_name, Some(("access", access_tok)))
       }
     } catch {
-        case StatusCode(401, _) =>  ("Rats! That PIN %s doesn't seem to match." format pin, None)
+        case StatusCode(401, _) =>  ("rats! that pin %s doesn't seem to match." format pin, None)
     }
 
     // this time we are matching against a potential request token
@@ -137,20 +156,21 @@ class TwitterProcessor extends Processor {
           dsk.getMethod("browse", classOf[java.net.URI]).invoke(
             dsk.getMethod("getDesktop").invoke(null), auth_uri
           )
-          "Accept the authorization request in your browser, for the fun to begin."
+          "accept the authorization request in your browser, for the fun to begin."
         } catch {
           // THAT went well. We'll just have to pass on that link to be printed.
           case _ =>
-            "Open the following URL in a browser to permit this application to tweet 4 u:\n%s".
+            "open the following URL in a browser to permit this application to tweet 4 u:\n%s".
               format(auth_uri.toString)
         }) + // so take one of those two messages and a general message
-          "\n\nThen run `twt <pin>` to complete authorization.\n",
+          "\n\nthen run `twt pin <pin>` to complete the process.\n",
           // and the request token that we got back from Twitter
           Some(("request", tok)))
     }) match // against the tuple produced by the last match
     {
       // no token, just a message to be printed up in `main`
-      case (message, None) => message
+      case (message, None) => println(message)
+
       // a token of some kind: we should save this in the datastore perhaps
       case (message, Some((name, tok))) =>
         val conf_writer = new java.io.FileWriter(conf)
@@ -166,7 +186,7 @@ class TwitterProcessor extends Processor {
             |</%s>""".stripMargin format (name, tok.value, tok.secret, name)
         )
         conf_writer.close
-        message // for you sir!
+        println(message) // for you sir!
     }
   }         // get_authorization
 
@@ -186,10 +206,16 @@ class TwitterProcessor extends Processor {
     val it = input.elements
     var state: ScanState = NoWord
 
-    def grab(st: ScanState) {
+    def grabQuoted {
       words append Quoted(word.toString)
       word = new StringBuilder
-      state = st
+      state = EndWord
+    }
+
+    def grabUnquoted {
+      words append Unquoted(word.toString)
+      word = new StringBuilder
+      state = NoWord
     }
 
     def unescape(c: Char) {
@@ -215,13 +241,13 @@ class TwitterProcessor extends Processor {
         case (x, NoWord) if x.isWhitespace  => // nothing
         case (x, NoWord)                    => word += x
                                                state = InWord
-        case (x, InSQ) if x == '\''         => grab(EndWord)
+        case (x, InSQ) if x == '\''         => grabQuoted
         case (x, InSQ)                      => unescape(x)
-        case (x, InDQ) if x == '\"'         => grab(EndWord)
+        case (x, InDQ) if x == '\"'         => grabQuoted
         case (x, InDQ)                      => unescape(x)
         case (x, InWord) if x == '\''       => state = ScanError("Unexpected \'")
         case (x, InWord) if x == '\"'       => state = ScanError("Unexpected \"")
-        case (x, InWord) if x.isWhitespace  => grab(NoWord)
+        case (x, InWord) if x.isWhitespace  => grabUnquoted
         case (x, InWord)                    => word += x
         case (x, EndWord) if x.isWhitespace => state = NoWord
         case (x, EndWord)                   => state = ScanError("Expected space but met " + x)
